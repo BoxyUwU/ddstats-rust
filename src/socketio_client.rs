bb4f7e7825d7b09e989d2eff2a86ada3da104702
Boxy <rust@boxyuwu.dev>
2025-06-23 19:18:48 +0100

    Use rustls instead of openssl

--- src/socketio_client.rs
+++ src/socketio_client.rs
@@ -2,23 +2,28 @@
 // SocketIO Client - socketio_client.rs
 //
 
-use std::time::{Duration, Instant};
-use ddcore_rs::models::{GameStatus, StatsBlockWithFrames};
-use native_tls::{Protocol, TlsConnector};
+use crate::{
+    client::ConnectionState,
+    threads::{State, AAS},
+};
 use anyhow::Result;
+use ddcore_rs::models::{GameStatus, StatsBlockWithFrames};
 use num_traits::FromPrimitive;
-use websocket::{ClientBuilder, Message, sync::{Client, stream::NetworkStream}};
-use crate::{client::ConnectionState, threads::{AAS, State}};
+use std::{
+    net::TcpStream,
+    time::{Duration, Instant},
+};
+use tungstenite::{stream::MaybeTlsStream, Message, Utf8Bytes, WebSocket};
 
 /////////////////////////////////
 
 #[derive(Debug, PartialEq, PartialOrd)]
 pub enum SioStatus {
-	Disconnected = 1,
-	Connecting,
-	Connected,
-	LoggedIn,
-	Timeout,
+    Disconnected = 1,
+    Connecting,
+    Connected,
+    LoggedIn,
+    Timeout,
 }
 
 pub struct LiveGameClient {
@@ -37,7 +42,9 @@ impl LiveGameClient {
         tokio::spawn(async move {
             let cfg = crate::config::cfg();
             let mut msg_bus = state.load().msg_bus.0.subscribe();
-            let mut lgc = LiveGameClient { sio_status: SioStatus::Disconnected };
+            let mut lgc = LiveGameClient {
+                sio_status: SioStatus::Disconnected,
+            };
             let mut current_socket = None;
             let mut sio_tick_interval = tokio::time::interval(Duration::from_secs_f32(1. / 3.));
             let mut last_ping = Instant::now();
@@ -45,102 +52,101 @@ impl LiveGameClient {
             let mut login_cooldown = Instant::now();
 
             loop {
-
                 tokio::select! {
-                    msg = msg_bus.recv() => if let Ok(crate::threads::Message::SocketIoMessage(data)) = msg {
-                        new_sio_message = Some(data);
-                    },
-                    _elapsed = sio_tick_interval.tick() => {
-                        let state = state.load();
-                        let last_data = &state.last_poll;
-                        if (*state.conn).eq(&ConnectionState::Connected) {
-                            if lgc.sio_status.ne(&SioStatus::LoggedIn) {
-                                if Instant::now().duration_since(login_cooldown) > Duration::from_secs(2) {
-                                    login_cooldown = Instant::now();
-                                    let connection = connect();
-                                    if connection.is_ok() {
-                                        current_socket = Some(connection.unwrap());
-                                        lgc.sio_status = SioStatus::Connected;
-                                        let res = current_socket.as_mut().unwrap().send_message(&Message::text(create_login_message(last_data.block.player_id)));
-                                        if res.is_ok() {
-                                            lgc.sio_status = SioStatus::LoggedIn;
-                                        }
+                msg = msg_bus.recv() => if let Ok(crate::threads::Message::SocketIoMessage(data)) = msg {
+                    new_sio_message = Some(data);
+                },
+                _elapsed = sio_tick_interval.tick() => {
+                    let state = state.load();
+                    let last_data = &state.last_poll;
+                    if (*state.conn).eq(&ConnectionState::Connected) {
+                        if lgc.sio_status.ne(&SioStatus::LoggedIn) {
+                            if Instant::now().duration_since(login_cooldown) > Duration::from_secs(2) {
+                                login_cooldown = Instant::now();
+                                let connection = connect();
+                                if connection.is_ok() {
+                                    current_socket = Some(connection.unwrap());
+                                    lgc.sio_status = SioStatus::Connected;
+                                    let res = current_socket.as_mut().unwrap().send(Message::Text(Utf8Bytes::from(&create_login_message(last_data.block.player_id))));
+                                    if res.is_ok() {
+                                        lgc.sio_status = SioStatus::LoggedIn;
                                     }
-                                }
-                            } else {
-                                if let Some(submit_evt) = &new_sio_message {
-                                    let mut notify_pb = cfg.discord.notify_player_best;
-                                    let mut notify_above_1000 = cfg.discord.notify_above_1000;
-
-                                    if last_data.block.is_replay {
-                                        notify_pb = false;
-                                        notify_above_1000 = false;
-                                    }
-
-                                    log::info!("Submitting SIO {}", create_sio_submit(submit_evt, (notify_pb, notify_above_1000)));
-                                    let sio_submit = current_socket.as_mut().unwrap().send_message(&Message::text(create_sio_submit(submit_evt, (notify_pb, notify_above_1000))));
-                                    new_sio_message = None;
-                                    if sio_submit.is_err() {
-                                        current_socket = None;
-                                        lgc.sio_status = SioStatus::Disconnected;
-                                        continue;
-                                    }
-                                }
-
-                                // KeepAlive
-                                if last_ping.elapsed() > Duration::from_secs(24) {
-                                    let ping = current_socket.as_mut().unwrap().send_message(&Message::text("2"));
-                                    last_ping = Instant::now();
-                                    if ping.is_err() {
-                                        current_socket = None;
-                                        lgc.sio_status = SioStatus::Disconnected;
-                                        continue;
-                                    }
-                                }
-
-                                if last_data.block.is_in_game || last_data.block.status == GameStatus::Dead as i32 {
-                                    let mut death_type = -2;
-                                    if last_data.block.status == GameStatus::Playing as i32 {
-                                        death_type = -1;
-                                    } else if last_data.block.status == GameStatus::Dead as i32 {
-                                        death_type = last_data.block.death_type as i32;
-                                    }
-
-                                    if should_submit_sio(last_data) {
-                                        let res = current_socket.as_mut().unwrap().send_message(&Message::text(create_submit_stats_message(last_data, death_type)));
-                                        if res.is_err() {
-                                            current_socket = None;
-                                            lgc.sio_status = SioStatus::Disconnected;
-                                            continue;
-                                        }
-                                    }
-                                } else {
-                                    let status: GameStatus = FromPrimitive::from_i32(last_data.block.status).unwrap();
-                                    let sio_status = match status {
-                                        GameStatus::Title | GameStatus::Menu => 4,
-                                        GameStatus::Lobby => 5,
-                                        GameStatus::Playing => 2,
-                                        GameStatus::Dead => 6,
-                                        _ => 3,
-                                    };
-                                    if cfg.stream.stats {
-                                        let res = current_socket.as_mut().unwrap().send_message(&Message::text(create_change_status_message(last_data.block.player_id, sio_status)));
-                                        if res.is_err() {
-                                            current_socket = None;
-                                            lgc.sio_status = SioStatus::Disconnected;
-                                            continue;
-                                        }
-                                    }
-
                                 }
                             }
                         } else {
-                            current_socket = None;
-                            lgc.sio_status = SioStatus::Disconnected;
+                            if let Some(submit_evt) = &new_sio_message {
+                                let mut notify_pb = cfg.discord.notify_player_best;
+                                let mut notify_above_1000 = cfg.discord.notify_above_1000;
+
+                                if last_data.block.is_replay {
+                                    notify_pb = false;
+                                    notify_above_1000 = false;
+                                }
+
+                                log::info!("Submitting SIO {}", create_sio_submit(submit_evt, (notify_pb, notify_above_1000)));
+                                let sio_submit = current_socket.as_mut().unwrap().send(Message::Text(Utf8Bytes::from(&create_sio_submit(submit_evt, (notify_pb, notify_above_1000)))));
+                                new_sio_message = None;
+                                if sio_submit.is_err() {
+                                    current_socket = None;
+                                    lgc.sio_status = SioStatus::Disconnected;
+                                    continue;
+                                }
+                            }
+
+                            // KeepAlive
+                            if last_ping.elapsed() > Duration::from_secs(24) {
+                                let ping = current_socket.as_mut().unwrap().send(Message::Text(Utf8Bytes::from_static("2")));
+                                last_ping = Instant::now();
+                                if ping.is_err() {
+                                    current_socket = None;
+                                    lgc.sio_status = SioStatus::Disconnected;
+                                    continue;
+                                }
+                            }
+
+                            if last_data.block.is_in_game || last_data.block.status == GameStatus::Dead as i32 {
+                                let mut death_type = -2;
+                                if last_data.block.status == GameStatus::Playing as i32 {
+                                    death_type = -1;
+                                } else if last_data.block.status == GameStatus::Dead as i32 {
+                                    death_type = last_data.block.death_type as i32;
+                                }
+
+                                if should_submit_sio(last_data) {
+                                    let res = current_socket.as_mut().unwrap().send(Message::Text(Utf8Bytes::from(&create_submit_stats_message(last_data, death_type))));
+                                    if res.is_err() {
+                                        current_socket = None;
+                                        lgc.sio_status = SioStatus::Disconnected;
+                                        continue;
+                                    }
+                                }
+                            } else {
+                                let status: GameStatus = FromPrimitive::from_i32(last_data.block.status).unwrap();
+                                let sio_status = match status {
+                                    GameStatus::Title | GameStatus::Menu => 4,
+                                    GameStatus::Lobby => 5,
+                                    GameStatus::Playing => 2,
+                                    GameStatus::Dead => 6,
+                                    _ => 3,
+                                };
+                                if cfg.stream.stats {
+                                    let res = current_socket.as_mut().unwrap().send(Message::Text(Utf8Bytes::from(&create_change_status_message(last_data.block.player_id, sio_status))));
+                                    if res.is_err() {
+                                        current_socket = None;
+                                        lgc.sio_status = SioStatus::Disconnected;
+                                        continue;
+                                    }
+                                }
+
+                            }
                         }
+                    } else {
+                        current_socket = None;
+                        lgc.sio_status = SioStatus::Disconnected;
                     }
-                    }
-                };
+                }
+                }
+            }
         });
     }
 }
@@ -156,7 +162,10 @@ fn should_submit_sio(data: &StatsBlockWithFrames) -> bool {
 }
 
 fn create_sio_submit(ev: &SubmitSioEvent, (notify_pb, notify_above_1000): (bool, bool)) -> String {
-    format!("42[\"game_submitted\",{},{},{}]", ev.game_id,notify_pb, notify_above_1000)
+    format!(
+        "42[\"game_submitted\",{},{},{}]",
+        ev.game_id, notify_pb, notify_above_1000
+    )
 }
 
 fn create_change_status_message(player_id: i32, status: i32) -> String {
@@ -165,7 +174,8 @@ fn create_change_status_message(player_id: i32, status: i32) -> String {
 
 fn create_submit_stats_message(data: &StatsBlockWithFrames, death: i32) -> String {
     let cfg = crate::config::cfg();
-    format!("42[\"submit\",{},{:.4},{},{},{},{},{},{},{:.4},{:.4},{:.4},{},{},{},{}]",
+    format!(
+        "42[\"submit\",{},{:.4},{},{},{},{},{},{},{:.4},{:.4},{:.4},{},{},{},{}]",
         data.block.player_id,
         data.block.time,
         data.block.gems_total,
@@ -188,13 +198,9 @@ fn create_login_message(player_id: i32) -> String {
     format!("42[\"login\", {}]", player_id)
 }
 
-fn connect() -> Result<Client<Box<dyn NetworkStream + Send>>> {
-    let tls_connector =  TlsConnector::builder()
-        .min_protocol_version(Some(Protocol::Tlsv12))
-        .max_protocol_version(Some(Protocol::Tlsv12))
-        .build()?;
+fn connect() -> Result<WebSocket<MaybeTlsStream<TcpStream>>> {
+    let (websocket, _) =
+        tungstenite::connect("wss://ddstats.com/socket.io/?EIO=3&transport=websocket")?;
 
-    ClientBuilder::new("wss://ddstats.com/socket.io/?EIO=3&transport=websocket")?
-        .connect(Some(tls_connector))
-        .map_err(|_| anyhow::Error::msg("Websocket Error"))
+    Ok(websocket)
 }
